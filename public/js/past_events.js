e91905a216ca4c451f98d2bf9f8a4089ebe5353a
Alkalote <witherkiller1109@gmail.com>
2024-10-24 16:57:55 +0200

    Début de modal pour la description d'un évènement

--- public/js/past_events.js
+++ public/js/past_events.js
@@ -15,13 +15,13 @@ function showEventCards(events){
         img = findImgById(event.image.path);
 
         eventCard.innerHTML= `
-
-        <div style='background-image: url(${img});'>
-        <h3>${event.title}</h3>
-        <p> Organisé le <span>${event.createdAt}</span></p>
-        <button class="modalEvent" data-index="${index} >Plus d'information ici</button>
-        </div>
-
+            <div class='cardE'>
+            <div class = 'cardE-background' style='background-image: url(${img});'>
+            <h3 class='cardE-title'>${event.title}</h3>
+            <p class='cardE-p'> Organisé le <span>${event.createdAt}</span></p>
+            <button class="modalEvent" data-index="${index} >Plus d'information ici</button>
+            </div>
+            </div>
         `;
         eventList.push(event);
 
@@ -46,7 +46,15 @@ function showEventCards(events){
 
 function modalOpen(index){
 
-    
+    const event = eventBoard[index];
+    const eventDiv = document.querySelector('.eventDetails');
+    eventDiv.innerHTML=`        
+        <div><h2 class='modalE-title'>${event.title}</h2><img src=${event.image.path}></div>
+        <div class = 'modalE-background' style='background-image: url(${img});'>
+        <p class='modalE-p'> Organisé le <span>${event.createdAt}</span></p>
+        <p class='modalE-p'><span>${event.description}</span></p>
+        </div>
+        `;
 
 
 }
