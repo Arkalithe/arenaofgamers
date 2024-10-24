6a14c3bcb81e389ce4871add6d342bdb9bf3e350
FGuedouah <fguedouah.sio@gmail.com>
2024-10-24 16:15:11 +0200

    ajout de style

---
+++ public/js/contact.js
@@ -0,0 +1,14 @@
+let formContact = document.getElementById("contact");
+
+function valideForm() {
+  const name = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  const email = document.getElementById("email").value;
+  const object
+
+  if (!name || !email || !phone) {
+    alert("Remplissez tous les champs");
+  } else {
+    alert("Inscription validée");
+  }
+}
